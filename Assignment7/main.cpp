83b71736a9da16f5721bc7b5dcb1ddeb226564c4
Jasmine Jans <jasminejans@bkba-14474ad.gonzaga.edu>
2014-04-14 21:12:30 -0700

    Initial Commit

---
+++ Assignment7/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment7
+//
+//  Created by Jasmine Jans on 4/14/14.
+//  Copyright (c) 2014 Jasmine Jans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
